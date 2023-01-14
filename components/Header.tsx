df1db82aa8dd59555ec1801444bdb410780466d3
Captain Price <murathoncu@gmail.com>
2023-01-15 01:48:14 +0300

    add framer motion and done header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <header>
-      <div className="flex flex-row items-center">
+    <header className="sticky top-0 p-5 flex items-center justify-between max-w-7xl mx-auto z-20 xl:items-center ">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center">
         {/* Social Icons */}
         <SocialIcon
           url="https://github.com/murathudavendigar"
@@ -28,16 +43,32 @@ const Header = (props: Props) => {
           fgColor="gray"
           bgColor="transparent"
         />
-      </div>
-      <div>
+      </motion.div>
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer">
         <SocialIcon
           className="cursor-pointer"
           network="email"
           fgColor="gray"
           bgColor="transparent"
         />
-        <p className="uppercase">Get In Touch</p>
-      </div>
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get In Touch
+        </p>
+      </motion.div>
     </header>
   );
 };
