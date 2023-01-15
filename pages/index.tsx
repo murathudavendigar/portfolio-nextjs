348918dd3c079b82851d4243d18dd465c2dac2d4
Captain Price <murathoncu@gmail.com>
2023-01-16 02:13:44 +0300

    projects page done

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import About from "@/components/About";
 import Experience from "@/components/Experience";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Head from "next/head";
 
@@ -31,6 +32,9 @@ const Home = () => {
       </section>
 
       {/* Projects  */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
       {/* Contact Me  */}
     </div>
