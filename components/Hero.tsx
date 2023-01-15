47cd7d14166bde3ad465581289f9f9140f44b834
Captain Price <murathoncu@gmail.com>
2023-01-15 03:24:00 +0300

    hero section done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
@@ -7,11 +8,7 @@ type Props = {};
 
 const Hero = (props: Props) => {
   const [text, count] = useTypewriter({
-    words: [
-      "Hi, I am Murat Hüdavendigâr",
-      "Frontend Developer",
-      "React Developer",
-    ],
+    words: ["Hi, I am Murat Hüdavendigâr", "Captain Price", "React Developer"],
     loop: true,
     delaySpeed: 2000,
   });
@@ -26,10 +23,30 @@ const Hero = (props: Props) => {
         height={128}
         loading="lazy"
       />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#f7ab0a" />
-      </h1>
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Frontend Developer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#f7ab0a" />
+        </h1>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
