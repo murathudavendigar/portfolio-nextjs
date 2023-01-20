7add2346674cdf1c8068b915d8774151a9eaa62a
Captain Price <murathoncu@gmail.com>
2023-01-20 04:00:57 +0300

    add projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -37,4 +37,48 @@ export const projectsData: Projects[] = [
     language: "React.JS",
     github: "https://github.com/murathudavendigar/weather-app-react-typescript",
   },
+  {
+    img: "https://media.licdn.com/dms/image/C4D22AQG8vRoTz1pI0A/feedshare-shrink_800/0/1668135437209?e=1677110400&v=beta&t=lCMDyey6gaY_p3Mi26xBvvVJZLMeTaX31OAWZU2dlb4",
+    name: "Movie App",
+    description:
+      "React Movie App is a web app where you can search for movies, watch movie trailers or find recent movies after you sign up for the app.",
+    url: "https://murathudavendigar.github.io/movie-app-react/",
+    language: "React.JS",
+    github: "https://github.com/murathudavendigar/movie-app-react",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/109613328/205906176-255864ce-32b9-4f58-9217-a5bdc2326f9b.gif",
+    name: "Bored App",
+    description:
+      "React Bored App is a web application that when you're bored, randomly recommends activities based on your chosen criteria.",
+    url: "https://bored-api-react.netlify.app/",
+    language: "React.JS",
+    github: "https://github.com/murathudavendigar/bored-api-react",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/109613328/208204716-ce99c3af-0e59-4dae-bdfa-0a19e7a8ac44.gif",
+    name: "TypeScript Quiz App",
+    description:
+      "React TypeScript Quiz App is a fun application that uses TypeScript in its construction, consists of 10 questions and each correct answer earns 1 point.",
+    url: "https://react-typescript-quiz-app-nu.vercel.app/",
+    language: "React.JS",
+    github: "https://github.com/murathudavendigar/react-typescript-quiz-app",
+  },
+  {
+    img: "https://user-images.githubusercontent.com/109613328/213595256-f89568d3-3a9f-4d9e-93b9-9ba84287f6f5.png",
+    name: "Linktree Clone",
+    description: "Linktree clone website with Next.JS",
+    url: "https://linktree-nextjs-phi.vercel.app/",
+    language: "Next.JS",
+    github: "https://github.com/murathudavendigar/linktree-nextjs",
+  },
+  {
+    img: "https://camo.githubusercontent.com/0f30b41c5edd5f932987b7865966241537e77ca9db81b3e9f7153c26b99e3a21/68747470733a2f2f6d656469612e6c6963646e2e636f6d2f646d732f696d6167652f433444323241514731544e506b5056574e68772f6665656473686172652d736872696e6b5f3830302f302f313636393832333537363632383f653d3136373539303038303026763d6265746126743d714f5a4f523978596535662d6a6d4c724176744574577851492d6f4b346d56634b3249306b6d575f327730",
+    name: "Fireblog App",
+    description:
+      "React Fireblog App is a web application where you can read and add blogs after registering with the app.",
+    url: "https://murathudavendigar.github.io/fireblog-app-react/",
+    language: "React.JS",
+    github: "https://github.com/murathudavendigar/fireblog-app-react",
+  },
 ];
