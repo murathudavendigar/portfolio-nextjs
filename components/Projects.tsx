8a6bfb2d58816525efeaa1cb3e27c87131c3152c
Captain Price <murathoncu@gmail.com>
2023-01-20 03:48:25 +0300

    fixed responsive

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -73,14 +73,14 @@ const Projects = (props: Props) => {
               />
             </a>
             <div className="space-y-6 px-0 md:px-10 max-w-6xl">
-              <h4 className="text-4xl font-semibold text-center">
+              <h4 className="text-2xl md:text-4xl font-semibold text-center">
                 <span className=" border-b border-[#CA3E47] ">
                   <span>{lang}</span> Study {index + 1} of {showProjects.length}
                   :{" "}
                 </span>
                 {project.name}
               </h4>
-              <p className="text-lg text-center md:text-left">
+              <p className="text-sm md:text-lg text-center md:text-left">
                 {project.description}
               </p>
               <div className="flex justify-center">
