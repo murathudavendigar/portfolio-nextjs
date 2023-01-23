983f6011cb0edd768e4578659beb998951930823
Captain Price <murathoncu@gmail.com>
2023-01-24 02:02:49 +0300

    changed font color

--- components/About.tsx
+++ components/About.tsx
@@ -10,7 +10,7 @@ const About = (props: Props) => {
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
       className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl ">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-200 text-2xl ">
         About
       </h3>
       <motion.img
